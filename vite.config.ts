80ec8028c3b6c9ade25d58d27a8852c97650c11a
anarose314 <dpgp706@gmail.com>
2026-03-20 11:58:27 +0900

    🔧 Chore: Tailwind CSS v4 설치 및 Vite 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
