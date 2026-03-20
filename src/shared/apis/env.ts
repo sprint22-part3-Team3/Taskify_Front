d1783fb4e42989a95800fd7166a719db441c9b9e
GENIE\김지니상 <hgenie0604@gmail.com>
2026-03-20 22:38:44 +0900

    ♻️ Refactor: 코드리뷰 사항 반영하여 수정 커밋

--- src/shared/apis/env.ts
+++ src/shared/apis/env.ts
@@ -1,3 +1,7 @@
+const API_TEAM_BASE_URL = import.meta.env.VITE_API_TEAM_BASE_URL;
+if (!API_TEAM_BASE_URL) {
+  throw new Error('VITE_API_TEAM_BASE_URL is not defined');
+}
 export const ENV = {
-  API_TEAM_BASE_URL: import.meta.env.VITE_API_TEAM_BASE_URL as string,
-};
+  API_TEAM_BASE_URL,
+} as const;
