f67cb15724ed035c96728d8da1af51fe39e7ef25
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-25 01:47:16 +0900

    ✨ Feat: main footer 구현

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,17 @@
+import MainFooter from '@/pages/main/components/main-footer';
+import HeroSection from '@/pages/main/components/hero-section';
+import MainHeader from '@/pages/main/components/main-header';
+
 function MainPage() {
-  return <div>Main Page</div>;
+  return (
+    <div className="flex min-h-screen flex-col bg-white">
+      <MainHeader />
+      <div className="flex-1">
+        <HeroSection />
+      </div>
+      <MainFooter />
+    </div>
+  );
 }
 
 export default MainPage;
