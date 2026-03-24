24cd20a71a39213d1633c011274b6756d4e06696
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-25 05:13:38 +0900

    ✨ Feat: auth layout 구현

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthLayout from '@/features/auth/components/auth-layout';
 import DashboardDetailPage from '@/pages/dashboard-detail';
 import DashboardEditPage from '@/pages/dashboard-edit';
 import LoginPage from '@/pages/login';
@@ -12,8 +13,10 @@ function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Route>
         <Route path="/mydashboard" element={<MyDashboardPage />} />
         <Route path="/dashboard/:id" element={<DashboardDetailPage />} />
         <Route path="/dashboard/:id/edit" element={<DashboardEditPage />} />
---
+++ src/features/auth/components/auth-layout/index.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router';
+
+export default function AuthLayout() {
+  return (
+    <div className="min-h-dvh bg-gray-50 px-3 py-20 sm:py-40">
+      <main className="mx-auto flex w-full max-w-140 flex-col items-center">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
