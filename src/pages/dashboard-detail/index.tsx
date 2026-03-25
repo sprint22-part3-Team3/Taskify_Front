2c2ef17dea3ef59fcc8d675ea0d3eb446be55ab8
anarose314 <dpgp706@gmail.com>
2026-03-25 21:58:20 +0900

    ♻️ Refactor: 대시보드 상세페이지 레이아웃 중복 스타일 제거

--- src/pages/dashboard-detail/index.tsx
+++ src/pages/dashboard-detail/index.tsx
@@ -1,12 +1,7 @@
 import { ColumnList } from '@/features/columns/components/column-list';
 
 function DashboardDetailPage() {
-  return (
-    // TODO : 전체 레이아웃과 중복되는 스타일 수정
-    <main className="flex min-h-screen min-w-max flex-col bg-gray-50">
-      <ColumnList />
-    </main>
-  );
+  return <ColumnList />;
 }
 
 export default DashboardDetailPage;
