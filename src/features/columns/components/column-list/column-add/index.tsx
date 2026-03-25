54fe4e0fd49339ab6687750c8d9bea25267ec12b
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-26 06:32:41 +0900

    ✨ Feat: 칼럼 생성 모달 구현

--- src/features/columns/components/column-list/column-add/index.tsx
+++ src/features/columns/components/column-list/column-add/index.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+import CreateColumnModal from '@/features/columns/components/modal/create-column-modal';
 import { AddItemButton } from '@/shared/components/add-item-button';
 
 function ColumnAdd() {
-  // TODO : 새로운 컬럼 추가하기 버튼 이벤트 핸들러 함수 작성
-  const handleAddColumn = () => {};
+  const [isCreateColumnModalOpen, setIsCreateColumnModalOpen] = useState(false);
+
+  const handleAddColumn = () => {
+    setIsCreateColumnModalOpen(true);
+  };
 
   return (
-    <AddItemButton onClick={handleAddColumn} className="h-16.5 md:h-17.5">
-      새로운 컬럼 추가하기
-    </AddItemButton>
+    <>
+      <AddItemButton onClick={handleAddColumn} className="h-16.5 md:h-17.5">
+        새로운 컬럼 추가하기
+      </AddItemButton>
+      <CreateColumnModal
+        isOpen={isCreateColumnModalOpen}
+        onClose={() => setIsCreateColumnModalOpen(false)}
+      />
+    </>
   );
 }
 
