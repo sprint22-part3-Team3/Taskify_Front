268c2981241f45bbd54182e145d47ce214de1090
anarose314 <dpgp706@gmail.com>
2026-03-25 11:04:29 +0900

    ✨ Feat: 컬럼 UI 퍼블리싱 및 대시보드 상세 페이지 레이아웃

--- src/pages/dashboard-detail/index.tsx
+++ src/pages/dashboard-detail/index.tsx
@@ -1,5 +1,12 @@
+import { ColumnList } from '@/features/columns/components/column-list';
+
 function DashboardDetailPage() {
-  return <div>Dashboard Detail Page</div>;
+  return (
+    // TODO : 전체 레이아웃과 중복되는 스타일 수정
+    <main className="flex min-h-screen min-w-max flex-col bg-gray-50">
+      <ColumnList />
+    </main>
+  );
 }
 
 export default DashboardDetailPage;
