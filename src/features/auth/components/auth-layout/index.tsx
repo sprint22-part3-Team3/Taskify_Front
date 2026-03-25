993d9478121fd335de5524704e4923864307cc3b
Lseojeong <sjung0314@gachon.ac.kr>
2026-03-26 02:42:00 +0900

    🎨 Style: auth layout py 값 조정

--- src/features/auth/components/auth-layout/index.tsx
+++ src/features/auth/components/auth-layout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router';
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-dvh bg-gray-50 px-3 py-40 md:py-20">
+    <div className="min-h-dvh bg-gray-50 px-3 py-27">
       <main className="mx-auto flex w-full max-w-140 flex-col items-center">
         <Outlet />
       </main>
