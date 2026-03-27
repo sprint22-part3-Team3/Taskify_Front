ad6ad30c1fa75e101f4d264e537e121894040ae8
anarose314 <dpgp706@gmail.com>
2026-03-27 20:09:50 +0900

    ♻️ Refactor: useColumnList에 ApiError 적용하여 404 에러 분기 로직 개선

--- src/features/columns/hooks/useColumnList.ts
+++ src/features/columns/hooks/useColumnList.ts
@@ -1,6 +1,7 @@
 import type { Column } from '@/features/columns/types/column.types';
 import { useEffect, useState } from 'react';
 import { getColumns } from '@/features/columns/apis/columns';
+import { ApiError } from '@/shared/apis/apiError';
 
 const NOT_FOUND_OR_FORBIDDEN_ERROR =
   '대시보드를 찾을 수 없거나 접근 권한이 없습니다';
@@ -33,7 +34,7 @@ export const useColumnList = (dashboardId: number) => {
             ? err.message
             : '알 수 없는 에러가 발생했습니다.';
 
-        if (message.includes('404')) {
+        if (err instanceof ApiError && err.status === 404) {
           setErrorMessage(NOT_FOUND_OR_FORBIDDEN_ERROR);
         } else {
           setErrorMessage(message);
