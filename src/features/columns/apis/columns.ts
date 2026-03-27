7aaeb9ffd583a041bf474f8b517fbcf4f6dafb05
anarose314 <dpgp706@gmail.com>
2026-03-27 19:59:37 +0900

    📝 Docs: getColumns에 공통 토큰 로직 관련 TODO 주석 추가

--- src/features/columns/apis/columns.ts
+++ src/features/columns/apis/columns.ts
@@ -7,6 +7,7 @@ import { get } from '@/shared/apis/fetchInstance';
 /**
  * GET 컬럼 목록 조회
  */
+// TODO : 공통 토큰 로직 추가 후 정리
 export const getColumns = async ({ dashboardId, token }: getColumnsParams) => {
   const res = await get<GetColumnsResponse>(
     `columns?dashboardId=${dashboardId}`,
