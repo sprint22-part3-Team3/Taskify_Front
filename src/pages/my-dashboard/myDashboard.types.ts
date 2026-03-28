d1458a379f07bdc58f618d76da8e12ef2ff09e1c
권새롬 <rhdtls3562@naver.com>
2026-03-28 16:43:46 +0900

    🐛 Fix: 브랜치 잘못 설정

--- src/pages/my-dashboard/myDashboard.types.ts
+++ src/pages/my-dashboard/myDashboard.types.ts
@@ -1,2 +1,13 @@
-export type { DashboardItem } from '@/features/dashboards/types/dashboard.types';
-export type { InvitedDashboardItem } from '@/features/invitations/types/invitation.types';
+import type { DashboardColorName } from '@/features/dashboards/types/dashboardColor.types';
+
+export type DashboardItem = {
+  id: number;
+  title: string;
+  color: DashboardColorName;
+};
+
+export type InvitedDashboardItem = {
+  id: number;
+  name: string;
+  inviter: string;
+};
