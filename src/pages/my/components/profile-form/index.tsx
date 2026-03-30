5804e42b20424ca8cba57ff0983ebda879a1cd32
hyewon <haeeon1123@gmail.com>
2026-03-31 00:12:24 +0900

    ♻️ Refactor: 이미지 업로드 수정되는 로직 변경

--- src/pages/my/components/profile-form/index.tsx
+++ src/pages/my/components/profile-form/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 
 import Title from '@/shared/components/title';
 import Input from '@/shared/components/input';
@@ -19,11 +19,20 @@ export default function ProfileForm() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [previewImageUrl, setPreviewImageUrl] = useState<string | undefined>(
+    undefined
+  );
 
   const isSaveDisabled =
     isSubmitting ||
     ((nickname.trim() === '' || nickname.trim() === user?.nickname) &&
       !selectedFile);
+
+  useEffect(() => {
+    if (user?.profileImageUrl) {
+      setPreviewImageUrl(user.profileImageUrl);
+    }
+  }, [user?.profileImageUrl]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -59,13 +68,10 @@ export default function ProfileForm() {
 
   const handleFileSelect = (file: File | null) => {
     if (!file) return;
-    const initialFileName = user?.profileImageUrl?.split('/').pop();
-    if (file.name === initialFileName) {
-      setSelectedFile(null);
-    } else {
-      setSelectedFile(file);
-    }
+    setSelectedFile(file);
+    setPreviewImageUrl(undefined);
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="mt-1 w-71 rounded-xl bg-white p-4 md:mt-7 md:w-137 md:p-6 lg:w-2xl">
@@ -80,8 +86,7 @@ export default function ProfileForm() {
 
         <div className="w-full md:flex md:justify-between">
           <ImageUploadBox
-            key={user?.profileImageUrl}
-            imageUrl={user?.profileImageUrl ?? undefined}
+            imageUrl={previewImageUrl}
             onFileSelect={handleFileSelect}
           />
           <div className="mt-10 md:mt-0 md:w-69 lg:w-100">
