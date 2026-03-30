9210d963d923825bcd5fa555cfc125781acb3eb1
hyewon <haeeon1123@gmail.com>
2026-03-30 23:03:40 +0900

    ♻️ Refactor: 이미지 업로드 develop 과 머지 후 충돌 수정

--- src/shared/components/image-uploader/imageUploader.types.ts
+++ src/shared/components/image-uploader/imageUploader.types.ts
@@ -2,4 +2,6 @@ export type ChangeEvent<T = Element> = React.ChangeEvent<T>;
 
 export type ImageUploadBoxProps = {
   variant?: 'default' | 'modal';
+  imageUrl?: string | null;
+  onFileSelect?: (file: File) => void;
 };
