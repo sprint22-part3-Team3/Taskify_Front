9928fa22ba8ae2dc5577e1dd377095ded191d432
hyewon <haeeon1123@gmail.com>
2026-03-30 18:51:58 +0900

    🚚 Rename: : 컬럼 타입 불러오는 파일 경로 수정

--- src/features/cards/components/card-list/card-list-header/cardListHeader.types.ts
+++ src/features/cards/components/card-list/card-list-header/cardListHeader.types.ts
@@ -1,4 +1,4 @@
-import type { Column } from '@/features/columns/types/column.types';
+import type { Column } from '@/features/columns/types/cloumn.types';
 
 export type CardListHeaderProps = Pick<Column, 'title' | 'id'> & {
   cardCount: number;
