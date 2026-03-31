7eb6b856c7632dfa3823253aecd7ddf88d4f5b76
anarose314 <dpgp706@gmail.com>
2026-03-31 20:36:56 +0900

    🐛 Fix: 댓글 날짜 포매팅 수정

--- src/features/comments/components/task-comments/task-comment-item/index.tsx
+++ src/features/comments/components/task-comments/task-comment-item/index.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/shared/components/button';
 import TextArea from '@/shared/components/text-area';
 import useResponsiveValue from '@/shared/hooks/useResponsiveValue';
 import { cn } from '@/shared/utils/cn';
-import { formatDateTimeValue } from '@/shared/utils/date';
+import { formatDateTimeValue, parseDateTimeValue } from '@/shared/utils/date';
 import { useState } from 'react';
 
 const TEXT_BUTTON_CLASS = cn(
@@ -22,6 +22,9 @@ const TEXT_BUTTON_CLASS = cn(
 function TaskCommentItem({ comment, refetch, onDelete }: TaskCommentItemProps) {
   const { id, author, createdAt, content } = comment;
 
+  const formattedDateTime = formatDateTimeValue(
+    parseDateTimeValue(createdAt.substring(0, 16))
+  );
   const avatarSize = useResponsiveValue<'md' | 'lg'>(TASK_MODAL_AVATAR_SIZE);
 
   const [isEditing, setIsEditing] = useState(false);
@@ -69,7 +72,7 @@ function TaskCommentItem({ comment, refetch, onDelete }: TaskCommentItemProps) {
             {author.nickname}
           </p>
           <p className="text-2xs md:typo-xs-regular text-gray-300">
-            {formatDateTimeValue(new Date(createdAt))}
+            {formattedDateTime}
           </p>
         </div>
 
