5c852c0134dfa648fc27917e963d2f11fd4aa96f
GENIE\김지니상 <hgenie0604@gmail.com>
2026-03-31 15:35:55 +0900

    ♻️ Refactor: 엔드포인트값 수정 및 null에서 void로 수정

--- src/features/dashboards/apis/deleteDashboard.ts
+++ src/features/dashboards/apis/deleteDashboard.ts
@@ -3,6 +3,6 @@ import { del } from '@/shared/apis/fetchInstance';
 /**
  * 대시보드를 삭제하는 함수
  */
-export async function deleteDashboard(dashboardId: string): Promise<null> {
-  return del<null>(`dashboards/${dashboardId}`);
+export async function deleteDashboard(dashboardId: string): Promise<void> {
+  await del(`dashboards/${dashboardId}`);
 }
