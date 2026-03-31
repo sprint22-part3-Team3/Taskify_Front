8bbe9872b68228a8896ae81c27ffa34889a527d8
GENIE\김지니상 <hgenie0604@gmail.com>
2026-03-31 15:42:32 +0900

    ♻️ Refactor: 엔드포인트값 재수정

--- src/features/dashboards/apis/deleteDashboard.ts
+++ src/features/dashboards/apis/deleteDashboard.ts
@@ -4,5 +4,5 @@ import { del } from '@/shared/apis/fetchInstance';
  * 대시보드를 삭제하는 함수
  */
 export async function deleteDashboard(dashboardId: string): Promise<void> {
-  await del(`dashboards/${dashboardId}`);
+  await del(`/dashboards/${dashboardId}`);
 }
